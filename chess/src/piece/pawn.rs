2d1505008cdbcdc37dfad4549508455e4a6e79dd
Zachery Johnson <zacheryjohnson13@gmail.com>
2020-12-27 23:41:51 -0800

    Add en passant captures for pawns

--- chess/src/piece/pawn.rs
+++ chess/src/piece/pawn.rs
@@ -1,13 +1,13 @@
 use crate::piece::{*};
+use crate::board::Rank;
 
 pub struct Pawn {
-  color: Color,
-  has_moved: bool,
+  color: Color
 }
 
 impl Pawn {
   pub fn new(color: Color) -> Pawn {
-    Pawn { color, has_moved: false }
+    Pawn { color }
   }
 }
 
@@ -20,17 +20,28 @@ impl Piece for Pawn {
     let forward: i8 = if self.color == Color::White { 1 } else { -1 };
     let forward_move: (i8, i8) = (0, forward);
 
-    let mut potential_moves: Vec<Coordinate> = vec![*own_coords + forward_move];
+    let mut potential_moves: Vec<Coordinate> = vec!();
+
+    let one_square_move = *own_coords + forward_move;
+    match board.get_square(one_square_move) {
+      Ok(square) if square.get_piece().as_ref().is_none() => { potential_moves.push(one_square_move); },
+      _ => {}
+    }
 
     // Pawns that haven't moved yet can move two squares
-    if !self.has_moved {
-      potential_moves.push(*own_coords + (forward_move.0, forward_move.1 * 2))
+    if *self.get_color() == Color::White && own_coords.rank == Rank::Two || *self.get_color() == Color::Black && own_coords.rank == Rank::Seven {
+      let two_square_move = *own_coords + (0, forward * 2);
+      match board.get_square(two_square_move) {
+        Ok(square) if square.get_piece().as_ref().is_none() => { potential_moves.push(two_square_move); },
+        _ => {}
+      }
     }
 
     let capture_squares: Vec<Coordinate> = vec![*own_coords + (1, forward), *own_coords + (-1, forward)];
     for capture in capture_squares {
       match board.get_square(capture) {
         Ok(square) if square.get_piece().as_ref().is_some() && square.get_piece().as_ref().unwrap().get_color() != self.get_color() => { potential_moves.push(capture); },
+        Ok(_) if board.get_en_passant_target().as_ref().is_some() && board.get_en_passant_target().as_ref().unwrap() == &capture => { potential_moves.push(capture); }
         _ => {},
       }
     }
@@ -53,6 +64,7 @@ mod tests {
       .get_piece().as_ref().unwrap()
       .get_moves(&board, &coords);
 
+    assert_eq!(moves.len(), 2);
     assert!(moves.contains(&Coordinate { file: File::C, rank: Rank::Three }));
     assert!(moves.contains(&Coordinate { file: File::C, rank: Rank::Four }));
   }
@@ -66,6 +78,7 @@ mod tests {
       .get_piece().as_ref().unwrap()
       .get_moves(&board, &coords);
 
+    assert_eq!(moves.len(), 2);
     assert!(moves.contains(&Coordinate { file: File::F, rank: Rank::Six }));
     assert!(moves.contains(&Coordinate { file: File::F, rank: Rank::Five }));
   }
@@ -79,6 +92,7 @@ mod tests {
       .get_piece().as_ref().unwrap()
       .get_moves(&board, &coords);
 
+    assert_eq!(moves.len(), 2);
     assert!(moves.contains(&Coordinate { file: File::D, rank: Rank::Five }));
     assert!(moves.contains(&Coordinate { file: File::E, rank: Rank::Five }));
   }
@@ -92,7 +106,25 @@ mod tests {
       .get_piece().as_ref().unwrap()
       .get_moves(&board, &coords);
 
+    assert_eq!(moves.len(), 1);
     assert!(moves.contains(&Coordinate { file: File::D, rank: Rank::Five }));
     assert!(!moves.contains(&Coordinate { file: File::E, rank: Rank::Five })); // This is an ally on E5 - assert that it cannot capture that piece
+  }
+
+  #[test]
+  fn test_capture_en_passant() {
+    let board = Board::from_fen_string("rnbqkbnr/ppp2ppp/3p4/3Pp3/8/8/PPP1PPPP/RNBQKBNR w KQkq e6 0 3").unwrap();
+    let coords = Coordinate { file: File::D, rank: Rank::Five };
+    let moves = board
+      .get_square(coords.clone()).unwrap()
+      .get_piece().as_ref().unwrap()
+      .get_moves(&board, &coords);
+
+    for m in &moves {
+      println!("{}", m);
+    }
+
+    assert_eq!(moves.len(), 1);
+    assert!(moves.contains(&Coordinate { file: File::E, rank: Rank::Six })); // En passant capture
   }
 }
